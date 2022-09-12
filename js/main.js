2ee1cbcaaf9482542110995895e84dee1d781fe2
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-12 22:53:14 +0000

    Listen for 'submit' events on the journal entry form + subtasks. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 var $photoURL = document.querySelector('#photo-url');
 var $img = document.querySelector('img');
+var $form = document.querySelector('form');
 
 $photoURL.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  var newEntry = {
+    title: document.querySelector('#title').value,
+    photoURL: document.querySelector('#photo-url').value,
+    notes: document.querySelector('#notes').value,
+    nextEntryId: data.nextEntryId
+  };
+
+  data.nextEntryId += 1;
+  data.entries.push(newEntry);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
